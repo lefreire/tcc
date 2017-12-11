408b6373ead45c3596aab8ce98060afd17087edb
lefreire <letfreirefigueiredo@gmail.com>
2017-12-11 20:10:43 -0200

    Adding same initial population

--- island/island_deap.py
+++ island/island_deap.py
@@ -17,11 +17,13 @@ from testsFlags import *
 #deap code
 #arguments for genetic function
 args = 5*[0]
-args[0] = 500 #number of generations
+args[0] = 50 #number of generations
 args[1] = 150 # size of population
 args[2] = 51 #size of individual
 args[3] = 0.3 #crossover rate
 args[4] = 0.1 #mutation rate
+individuos = []
+
 
 #starting with the same population
 pop = PopulationFlags(args[1])
@@ -30,6 +32,18 @@ genetic = GeneticFlags()
 lista = range(100000)
 #construct the first population
 pop.constructPop(lista, args[2], args[3], args[4])
+
+#taking the first population
+for ind in pop.population:
+    individual_first = []
+    for individual in ind.individual:
+        individual_first = individual_first + [individual[0]]
+    individuos = individuos + [individual_first]
+
+
+print "INDIVIDUOS: ", individuos
+
+
 #make TestsFlags object
 tests = TestsFlags(genetic, args[0], args[2], args[1], args[4], args[3])
 listGenerations = [args[0]]
@@ -38,3 +52,4 @@ tests.changePopSize(pop, listGenerations)
 
 #for pygmo evolution
 from my_problem import *
+main(individuos)
--- island/my_problem.py
+++ island/my_problem.py
@@ -15,47 +15,55 @@ class my_problem:
 
 
     def get_bounds(self):
-        return ([1] * self.dim, [10000] * self.dim)
+        return ([1] * self.dim, [100000] * self.dim)
 
-#defining some numbers
-no_pop = 150
-no_generations = 50
-no_individuals = 51
-#defining my problem
-prob = pg.problem(my_problem(no_individuals))
-#prob = pg.problem(pg.rosenbrock(dim = 4))
-print prob
 
-for i in range(0, 10):
-    #creating my population
-    pop = pg.population(prob)
-    #print pop
-    #generating the individuals
-    list_int = range(10000)
-    for i in range(0, no_pop):
-        individual = []
-        pos_list = randint(0, len(list_int)-1) #choose a random position in a list
-        for i in range(0, no_individuals):
-            individual =individual + [list_int[pos_list]]
-            pos_list = randint(0, len(list_int)-1) #choose a random position in a list
-        pop.push_back(x = individual)
-    #print pop
+def main(population):
+    print "APENAS UM TESTE: ", population
+    #defining some numbers
+    no_pop = 150
+    no_generations = 50
+    no_individuals = 51
+    #defining my problem
+    prob = pg.problem(my_problem(no_individuals))
+    #prob = pg.problem(pg.rosenbrock(dim = 4))
+    print prob
 
-    values = []
-    algo = pg.algorithm(pg.sga(gen=no_generations, cr=0.3, m=0.1, selection = "tournament"))
-    algo.set_verbosity(1)
-    #print algo
-    pop = algo.evolve(pop)
-    logs = algo.extract(pg.sga).get_log() 
-    print logs
-    #making the plot
-    plt.plot([l[0] for l in logs],[-l[2] for l in logs], linewidth=2.5,  label = -logs[-1][2])
-    plt.legend(bbox_to_anchor=(0.5, 0), loc=3,
-           ncol=2, mode="expand", borderaxespad=0.)
-    #naming the x and y label and putting the title
-    plt.xlabel('generations')
-    plt.ylabel('best evaluate')
-    plt.title('Generation with: population= ' + str(no_pop)
-                 + ' , number generation= '+ str(no_generations), bbox={'facecolor': '0.8', 'pad': 5})
-    #saving the figure
-    plt.savefig('my_fig_problem_'+str(no_generations)+'.png')
+    for i in range(0, 1):
+        #creating my population
+        pop = pg.population(prob)
+        #print pop
+        #generating the individuals
+        list_int = range(100000)
+        for i in range(0, no_pop):
+            #individual = []
+            #pos_list = randint(0, len(list_int)-1) #choose a random position in a list
+            #for i in range(0, no_individuals):
+               # individual =individual + [list_int[pos_list]]
+                #pos_list = randint(0, len(list_int)-1) #choose a random position in a list
+            #pop.push_back(x = individual)
+            pop.push_back(x = population[i])
+        print "POPULACAO: ", pop
+
+        values = []
+        algo = pg.algorithm(pg.sga(gen=no_generations, cr=0.3, m=0.1, selection = "tournament"))
+        algo.set_verbosity(1)
+        #print algo
+        pop = algo.evolve(pop)
+        logs = algo.extract(pg.sga).get_log() 
+        print logs
+        #making the plot
+        plt.plot([l[0] for l in logs],[-l[2] for l in logs], linewidth=2.5,  label = -logs[-1][2])
+        plt.legend(bbox_to_anchor=(0.5, 0), loc=3,
+               ncol=2, mode="expand", borderaxespad=0.)
+        #naming the x and y label and putting the title
+        plt.xlabel('generations')
+        plt.ylabel('best evaluate')
+        plt.title('Generation with: population= ' + str(no_pop)
+                     + ' , number generation= '+ str(no_generations), bbox={'facecolor': '0.8', 'pad': 5})
+        #saving the figure
+        #plt.savefig('my_fig_problem_'+str(no_generations)+'.png')
+        plt.savefig('graphicChanges/myfig_elitism_'+str(no_generations)+'.png')
+
+if __name__ == "__main__":
+    main()
