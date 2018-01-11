4d34559291a6623e8d037409cd439047e07d39ec
lefreire <letfreirefigueiredo@gmail.com>
2018-01-11 19:37:09 -0200

    Documenting the methods

--- framework/userFunctions.py
+++ framework/userFunctions.py
@@ -6,24 +6,27 @@ class UserOptimizer:
 
     @abc.abstractmethod
     def initialFlags(self, initialList):
-        """
-            Method to be implemented by the user
-            Method to get the initial list of flags to be used in the optimization
+        """Method to be implemented by the user
+            Set the initial list of flags to be used in the optimization
+        
+        Parameters
+        ----------
+        initialList : list
+            The list with initial flags
         """
         return
 
     @abc.abstractmethod
     def pathToCompile(self):
-    	"""
-    	    Method to be implemented by the user
+    	"""Method to be implemented by the user
     	    Method to have the path to the compiling file
+
     	"""
     	return
 
     @abc.abstractmethod
     def evaluationFunction(self):
-    	"""
-    	    Method to be implemented by the user
+    	"""Method to be implemented by the user
     	    Method to do the evaluation of the code to be optimized
     	"""
     	return
