b24a6b9d1bae3ec0164c23ec8fa6f66ca20e0fb1
lefreire <letfreirefigueiredo@gmail.com>
2018-02-01 20:32:16 -0200

    Fixing indentation

--- framework/test.py
+++ framework/test.py
@@ -3,42 +3,42 @@ import os, subprocess
 import time
 
 class Test(UserParameters):
-	""" Class to test the abstract class UserParameters 
-	"""
+    """ Class to test the abstract class UserParameters 
+    """
     def initialFlags(self, initialList):
-    	""" Implementing the method initialFlags.
-    	    This method needs to return a list with the preferential compilation flags to be used in the evaluation
-    	"""
+        """ Implementing the method initialFlags.
+            This method needs to return a list with the preferential compilation flags to be used in the evaluation
+        """
         print initialList  
 
     def pathToCompile(self):
         """ Implementing the method pathToCompile.
-    	    This method returns the path where can be found the file 
-    	    to compile the target code (Makefile or similar)
-    	""" 
+            This method returns the path where can be found the file 
+            to compile the target code (Makefile or similar)
+        """ 
         pathToMake = os.getcwd() + "/codeToTest"
         return pathToMake
 
     def argumentsToCompile(self):
-    	""" Implementing the method argumentsToCompile.
-    	    This method returns the command to compile the target code
-    	""" 
-    	command = "make"
-    	return command
+        """ Implementing the method argumentsToCompile.
+            This method returns the command to compile the target code
+        """ 
+        command = "make"
+        return command
 
     def argumentsToRunCode(self):
-    	""" Implementing the method argumentsToRunCode.
-    	    This method returns the command to run the executable file
-    	""" 
-    	command = "./nbody.gpp-3.gpp_run 50000000"
-    	return command
+        """ Implementing the method argumentsToRunCode.
+            This method returns the command to run the executable file
+        """ 
+        command = "./nbody.gpp-3.gpp_run 50000000"
+        return command
 
     def evaluationFunction(self):
-    	""" Implementing the method evaluationFunction.
-    	    This method returns the value to be optimized by the framework.
-    	    In this method, the user needs to run the executable file and get the value to pass
-    	    to the framework.
-    	""" 
+        """ Implementing the method evaluationFunction.
+            This method returns the value to be optimized by the framework.
+            In this method, the user needs to run the executable file and get the value to pass
+            to the framework.
+        """ 
         path = os.getcwd() + "/codeToTest"
         os.chdir(path)
         print "======= Changing directory ======="
