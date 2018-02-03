36d0ca6bd6d3c4b6ea13f6729e9240ea7911be1e
lefreire <letfreirefigueiredo@gmail.com>
2018-02-03 17:40:22 -0200

    Adding make and make clean

--- island/my_problem.py
+++ island/my_problem.py
@@ -20,17 +20,55 @@ class my_problem:
 
     def fitness(self, x):
         #return [-sum(x)]
-        x = Test()
-        y =  x.evaluationFunction(x)
+        print "X: ", x
+        test = Test()
+        pathCompile = test.pathToCompile()
+        commandCompile = test.argumentsToCompile()
+        print commandCompile
+        flags = test.initialFlags()
+        individual = self.changeFlags(x, flags)
+        commandCompile = commandCompile + " CXXFLAGS="+ individual
+
+        path = os.getcwd()
+        os.chdir(pathCompile)
+        print "======= Changing directory ======="
+        my_env = os.environ.copy()
+        my_env["PATH"] = "/usr/sbin:/sbin:" + my_env["PATH"]
+        p = subprocess.Popen(commandCompile, shell=True, stdout=subprocess.PIPE)
+        out, err =  p.communicate()
+        os.chdir(path)
+        print "======= Changing directory ======="
+ 
+        y =  test.evaluationFunction(x)
+
+        pathClean = test.pathToClean()
+        commandClean = test.argumentsToClean()
+
+        os.chdir(pathClean)
+        print "======= Changing directory ======="
+        my_env = os.environ.copy()
+        my_env["PATH"] = "/usr/sbin:/sbin:" + my_env["PATH"]
+        p = subprocess.Popen(commandClean, shell=True, stdout=subprocess.PIPE)
+        out, err =  p.communicate()
+        os.chdir(path)
+        print "======= Changing directory ======="
+
         return [y]
 
 
     def get_bounds(self):
         #return ([1] * self.dim, [100000] * self.dim)
         return ([1] * self.dim, [6] * self.dim)
+ 
+    def changeFlags(self, individual, options):
+        individual_def = ""
+        for ind in xrange(0, len(individual)):
+            print options
+            if individual[ind]:
+                indivual_def = individual_def + " " + options[ind]
+        return individual_def
 
-
-def main(population):
+def main(population, flag_option):
     print "APENAS UM TESTE: ", population
     #defining some numbers
     no_pop = 10
@@ -59,19 +97,19 @@ def main(population):
         print algo
         pop = algo.evolve(pop)
         logs = algo.extract(pg.sga).get_log() 
-        print logs
+        print "logs: ", logs
         #making the plot
-        #plt.plot([l[0] for l in logs],[-l[2] for l in logs], linewidth=2.5,  label  = "Pygmo: "+ str('%.2E' % Decimal(-logs[-1][2])))
-        #plt.legend(bbox_to_anchor=(0.5, 0), loc=3,
-         #      ncol=2, mode="expand", borderaxespad=0.)
+        plt.plot([l[0] for l in logs],[l[2] for l in logs], linewidth=2.5,  label  = "Pygmo: "+ str('%.2E' % Decimal(-logs[-1][2])))
+        plt.legend(bbox_to_anchor=(0.5, 0), loc=3,
+               ncol=2, mode="expand", borderaxespad=0.)
         #naming the x and y label and putting the title
-        #plt.xlabel('generations')
-        #plt.ylabel('best evaluate')
-        #plt.title('Generation with: population= ' + str(no_pop)
-         #            + ' , number generation= '+ str(no_generations), bbox={'facecolor': '0.8', 'pad': 5})
+        plt.xlabel('generations')
+        plt.ylabel('best evaluate')
+        plt.title('Generation with: population= ' + str(no_pop)
+                     + ' , number generation= '+ str(no_generations), bbox={'facecolor': '0.8', 'pad': 5})
         #saving the figure
-        #plt.savefig('my_fig_problem_'+str(no_generations)+'.png')
+        plt.savefig('my_fig_problem_'+str(no_generations)+'.png')
         #plt.savefig('graphicChanges/myfig_elitism_'+str(no_generations)+'.png')
-
+        #plt.show()
 if __name__ == "__main__":
     main()
