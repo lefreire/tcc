94e14e36156e8174945771d9152daef000e2b5a5
lefreire <letfreirefigueiredo@gmail.com>
2018-02-17 22:59:32 -0200

    Making methods

--- framework/COptimizer.py
+++ framework/COptimizer.py
@@ -1,45 +1,50 @@
 import random
-
-
-#deap code
-#arguments for genetic function
-args = 5*[0]
-args[0] = 50 #number of generations
-args[1] = 10 # size of population
-args[2] = 6 #size of individual
-args[3] = 0.3 #crossover rate
-args[4] = 0.1 #mutation rate
-individuos = []
-individuos_def = []
-options = ['-O', '-O1', '-O2', '-O3', '-O0', '-Os'];
-
-
-for ind in xrange(0, args[1]):
-    individual_first = []
-    for individual in xrange(0, args[2]):
-        #individual_bool = random.randint(0,1)
-        individual_first = individual_first + [random.randint(0,1)]
-    individuos = individuos + [individual_first]
-
-for ind in xrange(0, args[1]):
-    individual_first = []
-    for individual in xrange(0, args[2]):
-        if individuos[ind][individual]:
-            individual_first = individual_first + [options[individual]]
-    individuos_def = individuos_def + [individual_first]
-
-
-
-print "INDIVIDUOS: ", individuos_def
-
-
-#for pygmo evolution
-#from ../island/my_problem import *
+#importing method
 import sys
 sys.path.insert(0, '../island/')
-print sys
-from my_problem import * 
+from my_problem import *
+#importing user methods
 from test import *
-#main(individuos_def)
-main(individuos, initialFlags) 
-#main(individuos, options)
+
+class COpt():
+    
+    def __init__(self):
+        pass
+
+    def initialParameters(self):
+        args = 5*[0]
+        args[0] = 50 #number of generations
+        args[1] = 10 # size of population
+        args[2] = 6 #size of individual
+        args[3] = 0.3 #crossover rate
+        args[4] = 0.1 #mutation rate
+        return args
+
+    def getFlagOptions(self):
+        test = Test()
+        return test.initialFlags()
+
+    def constructFirstPopulation(self):
+        options = self.getFlagOptions()
+        no_options = len(options)
+        parameters = self.initialParameters()
+        individuos = []
+        for ind in xrange(0, parameters[1]):
+            individual_first = []
+            for individual in xrange(0, no_options):
+                #individual_bool = random.randint(0,1)
+                individual_first = individual_first + [random.randint(0,1)]
+            individuos = individuos + [individual_first]
+        return individuos
+
+    def runOptimization(self):
+        population = self.constructFirstPopulation()
+        flags = self.getFlagOptions()
+        main(population, flags) 
+        
+    #def main(self):
+     #   self.runOptimization()
+
+
+if __name__ == "__main__":
+    COpt().runOptimization()
