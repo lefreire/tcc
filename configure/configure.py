e7757aad7c119e7b51f6627925d886c2a6b38e26
lefreire <letfreirefigueiredo@gmail.com>
2018-03-04 12:17:43 -0300

    Adding a configure file

---
+++ configure/configure.py
@@ -0,0 +1,15 @@
+import ConfigParser
+
+def getClass():
+    Config = ConfigParser.ConfigParser()
+    Config.read('../configure/myfile.ini')
+    # print Config
+    # print Config.sections()
+    sections = Config.sections()
+    return Config.get('ClassImport', 'Name')
+
+def getClassName():
+    Config = ConfigParser.ConfigParser()
+    Config.read('../configure/myfile.ini')
+    sections = Config.sections()
+    return Config.get('ClassImport', 'Class')
