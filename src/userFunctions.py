d69f8792a12da2f2154d21e54084b0b5137f80c5
lefreire <letfreirefigueiredo@gmail.com>
2018-05-06 21:01:49 -0300

    Adding userFunctions to src folder

--- src/userFunctions.py
+++ src/userFunctions.py
@@ -8,7 +8,8 @@ class UserParameters:
     # @abc.abstractmethod
     def __init__(self):
         """Method to be implemented by the user
-            Set the initial list of flags to be used in the optimization
+            Set the initial list of flags to be used in the 
+            optimization
             Set the path to compile
             Set if the user wants to minize or maximize the problem
         Parameters
@@ -22,14 +23,34 @@ class UserParameters:
             Command indicating how to compile the code
             The default command is "make"
         clean_path: string
-            Path containing where is the file to clean the executable files
+            Path containing where is the file to clean the executable 
+            files
         clean_command: string
             Command indicating how to clean the executable files
             The default command is "make clean"
         min_max: value
-            A value representing if the user wants to minimize ou maximize the problem
+            A value representing if the user wants to minimize or 
+            maximize the problem
             For minimize, the value is 1 and maximize, -1
             The default is minimize
+        macro: string
+            String to indicate to the compiler the flags to be used.
+        method_optimization: string
+            String containing the name of the method to do 
+            the optimization.
+            The string can be: island.
+        dict_optimization: dictionary
+            This dictionary needs to have all information for the 
+            optimization method. 
+            For the island method, you must give these informations:
+                -no_generations: number of generations
+                -no_pop: population size
+                -individual_size: individual size; this size must be 
+                equal than the number of flags in the 
+                flags_list
+                -crossover_rate: crossover rate; must be between 0 
+                and 1
+                -mutation_rate: mutation rate; must be between 0 and 1
         """
         self.flags_list = ["-O1", "-O2", "-O3"]
         self.compile_path = os.getcwd()
@@ -37,12 +58,19 @@ class UserParameters:
         self.clean_path = os.getcwd()
         self.clean_command = "make clean"
         self.min_max = 1
+        self.macro = "CXXFLAGS"
+        self.method_optimization = "island"
+        self.dict_optimization = {"no_generations": 50, 
+                                  "no_pop": 100, 
+                                  "individual_size": 3, 
+                                  "crossover_rate": 0.3, 
+                                  "mutation_rate": 0.1}
         return
 
     @abstractmethod
     def arguments_to_run_code(self):
         """Method to be implemented by the user
-            Method to have the way to run the executable file
+            Method to have the way to run the executable file 
 
         Returns
         -------
