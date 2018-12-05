4cc52592ce57ff4be7c63284bcb9676719d44bc7
lefreire <letfreirefigueiredo@gmail.com>
2018-12-05 08:39:26 -0200

    Adding long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='PyCoptimizer',
     version='0.1',
     description='Python Code Optimization using compilation flags',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Leticia Figueiredo',
     author_email='letfreirefigueiredo@gmail.com',
     packages=find_packages(),
